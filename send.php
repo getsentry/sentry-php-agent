5135832856d9e11cbf4d36071b2a656166e566ed
Alex Bouma <alex@bouma.me>
2025-03-13 15:04:31 +0100

    wip

--- send.php
+++ send.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$socket = fsockopen('127.0.0.1', 5148, $errorNo, $errorMsg, 2);
-
 Sentry\init([
     'dsn' => 'https://8b9ee30b3d9541d3b38d2fa6ddf9c73b@o447951.ingest.us.sentry.io/5572016',
     'http_client' => new class implements Sentry\HttpClient\HttpClientInterface {
         public function sendRequest(Sentry\HttpClient\Request $request, Sentry\Options $options): Sentry\HttpClient\Response
         {
-            global $socket;
+            if (!$request->hasStringBody()) {
+                return new Sentry\HttpClient\Response(400, [], 'Request body is empty');
+            }
+
+            $socket = fsockopen('127.0.0.1', 5148, $errorNo, $errorMsg, 2);
+
+            if ($socket === false) {
+                return new Sentry\HttpClient\Response(500, [], "Socket error: {$errorNo} - {$errorMsg}");
+            }
 
             fwrite($socket, $request->getStringBody());
+
+            fclose($socket);
 
             return new Sentry\HttpClient\Response(202, [], '');
         }
@@ -22,6 +30,8 @@ Sentry\init([
 
 $startTime = microtime(true);
 
+Sentry\captureMessage('Hello world!');
+Sentry\captureMessage('Hello world!');
 Sentry\captureMessage('Hello world!');
 
 $endTime = microtime(true);
