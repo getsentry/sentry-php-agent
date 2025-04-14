b6c692304c411b8dfdad4c30342cab02934c936a
Alex Bouma <alex@bouma.me>
2025-04-14 16:03:20 +0200

    Add rate-limiter to forwarder

--- src/Envelope.php
+++ src/Envelope.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Sentry\Agent;
 
+use Sentry\EventType;
+
 /**
  * @internal
  */
@@ -36,6 +38,34 @@ class Envelope
         return null;
     }
 
+    public function getFirstItemType(): ?EventType
+    {
+        $header = $this->getFirstItemHeader();
+
+        if ($header === null) {
+            return null;
+        }
+
+        $parsedHeader = json_decode($header, true);
+
+        $type = null;
+
+        if (\is_array($parsedHeader) && !empty($parsedHeader['type']) && \is_string($parsedHeader['type'])) {
+            $type = $parsedHeader['type'];
+        }
+
+        switch ($type) {
+            case (string) EventType::event():
+                return EventType::event();
+            case (string) EventType::transaction():
+                return EventType::transaction();
+            case (string) EventType::checkIn():
+                return EventType::checkIn();
+            default:
+                return null;
+        }
+    }
+
     public function getData(): string
     {
         return $this->data;
@@ -50,5 +80,22 @@ class Envelope
         }
 
         return substr($this->data, 0, $position);
+    }
+
+    public function getFirstItemHeader(): ?string
+    {
+        $headerEndsAt = strpos($this->data, "\n");
+
+        if ($headerEndsAt === false) {
+            return null;
+        }
+
+        $itemHeaderEndsAt = strpos($this->data, "\n", $headerEndsAt + 1);
+
+        if ($itemHeaderEndsAt === false) {
+            return null;
+        }
+
+        return substr($this->data, $headerEndsAt + 1, $itemHeaderEndsAt - $headerEndsAt - 1);
     }
 }
