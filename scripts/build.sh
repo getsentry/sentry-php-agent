68e375625630375046df802cd6b21439f539725f
Alex Bouma <alex@bouma.me>
2025-09-22 12:02:38 +0200

    Build the phar with the correct version on version bump

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,16 @@ set -e
 # Move into the agent directory
 cd agent
 
-# Force the root version to allow for reproducible builds (we use 0.0.0 so it's clear this is not a real version)
-export COMPOSER_ROOT_VERSION=0.0.0
+# Check if we have a version argument
+if [ -z "$1" ]; then
+  # Set a default version if none is provided (we use 0.0.0 so it's clear this is not a real version)
+  VERSION="0.0.0"
+else
+  VERSION="$1"
+fi
+
+# Force the root version to allow for reproducible builds
+export COMPOSER_ROOT_VERSION=$VERSION
 
 # Install composer dependencies (without dev dependencies since they are not needed in a production build)
 composer install --prefer-dist --no-interaction --no-progress --no-dev
--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -5,3 +5,14 @@ if [ "$(uname -s)" != "Linux" ]; then
     echo "Please use the GitHub Action."
     exit 1
 fi
+
+# Ensure we are in the root directory of the project
+SCRIPT_DIR="$( dirname "$0" )"
+cd $SCRIPT_DIR/..
+
+# Get the old and new version from the command line arguments
+OLD_VERSION="${1}"
+NEW_VERSION="${2}"
+
+# Run a new phar build with the tagged version
+./scripts/build.sh $NEW_VERSION
