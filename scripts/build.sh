60bc6ecd0615d3a9bda5121204b2894a980fb15b
Alex Bouma <alex@bouma.me>
2025-09-22 10:14:07 +0200

    Fix dynamic values in phar build

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,10 @@ set -e
 # Move into the agent directory
 cd agent
 
-# Install composer dependencies
+# Force the root version to allow for reproducible builds (we use 0.0.0 so it's clear this is not a real version)
+export COMPOSER_ROOT_VERSION=0.0.0
+
+# Install composer dependencies (without dev dependencies since they are not needed in a production build)
 composer install --prefer-dist --no-interaction --no-progress --no-dev
 
 # Build the project using Box
