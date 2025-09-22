c3aed2883faf9d11ed1d4db7b54a224d80921b02
Alex Bouma <alex@bouma.me>
2025-09-22 13:06:32 +0200

    Improve the CLI output and interactions

--- agent/src/Console/Log.php
+++ agent/src/Console/Log.php
@@ -11,9 +11,19 @@ class Log
 {
     private const LEVEL_INFO = 'INFO';
     private const LEVEL_ERROR = 'ERROR';
+    private const LEVEL_DEBUG = 'DEBUG';
+
+    /**
+     * @var bool Whether to output debug messages or not
+     */
+    private static $verbose = false;
 
     private static function output(string $level, string $message): void
     {
+        if ($level === self::LEVEL_DEBUG && !self::$verbose) {
+            return;
+        }
+
         echo \sprintf("sentry-agent [%-19s] [%-5s] %s\n", date('Y-m-d H:i:s'), $level, $message);
     }
 
@@ -25,5 +35,15 @@ class Log
     public static function error(string $message): void
     {
         self::output(self::LEVEL_ERROR, $message);
+    }
+
+    public static function debug(string $message): void
+    {
+        self::output(self::LEVEL_DEBUG, $message);
+    }
+
+    public static function setVerbose(bool $verbose): void
+    {
+        self::$verbose = $verbose;
     }
 }
